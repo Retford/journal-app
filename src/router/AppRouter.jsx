9c08ff3fab9c165e27dfa985932f27d878a00bcb
Retford <retfordjb@gmail.com>
2024-10-28 15:28:36 -0500

    hook useCheckAuth added

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,21 +1,28 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { AuthRoutes } from '../auth/routes/AuthRoutes';
 import { JournalRoutes } from '../journal/routes/JournalRoutes';
-import { useSelector } from 'react-redux';
 import { CheckingAuth } from '../ui';
 
-export const AppRouter = () => {
-  const { status } = useSelector((state) => state.auth);
+import { useCheckAuth } from '../hooks/useCheckAuth';
 
+export const AppRouter = () => {
+  const status = useCheckAuth();
   if (status === 'checking') return <CheckingAuth />;
 
   return (
     <Routes>
+      {status === 'authenticated' ? (
+        <Route path='/*' element={<JournalRoutes />} />
+      ) : (
+        <Route path='/auth/*' element={<AuthRoutes />} />
+      )}
+
+      {<Route path='/*' element={<Navigate to='/auth/login' />} />}
       {/* Login y registro */}
-      <Route path='/auth/*' element={<AuthRoutes />} />
+      {/* <Route path='/auth/*' element={<AuthRoutes />} /> */}
 
       {/* JournalAPP */}
-      <Route path='/*' element={<JournalRoutes />} />
+      {/* <Route path='/*' element={<JournalRoutes />} /> */}
     </Routes>
   );
 };

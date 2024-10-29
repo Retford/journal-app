63f361efd81e65f16ea89a5d09b747a303285464
Retford <retfordjb@gmail.com>
2024-10-28 19:33:48 -0500

    new SideBarItem component added and notes displayed in the SideBar

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -33,6 +33,7 @@ export const startNewNote = () => {
 export const startLoadingNotes = () => {
   return async (dispatch, getState) => {
     const { uid } = getState().auth;
+
     if (!uid) throw new Error('El UID del usuario no existe');
 
     const notes = await loadNotes(uid);

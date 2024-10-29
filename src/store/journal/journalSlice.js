28fb3f648f1d24df2cba95ca3a6705cdaf72bf22
Retford <retfordjb@gmail.com>
2024-10-28 19:07:29 -0500

    find and connect to firebase and add notes to redux

--- src/store/journal/journalSlice.js
+++ src/store/journal/journalSlice.js
@@ -3,15 +3,25 @@ import { createSlice } from '@reduxjs/toolkit';
 export const journalSlice = createSlice({
   name: 'journal',
   initialState: {
-    isSaving: true,
+    isSaving: false,
     messageSaved: '',
     notes: [],
     active: null,
   },
   reducers: {
-    addNewEmptyNote: (state, action) => {},
-    setActiveNote: (state, action) => {},
-    setNotes: (state, action) => {},
+    savingNewNote: (state) => {
+      state.isSaving = true;
+    },
+    addNewEmptyNote: (state, action) => {
+      state.notes.push(action.payload);
+      state.isSaving = false;
+    },
+    setActiveNote: (state, action) => {
+      state.active = action.payload;
+    },
+    setNotes: (state, action) => {
+      state.notes = action.payload;
+    },
     setSaving: (state) => {},
     updateNote: (state, action) => {},
     deleteNoteById: (state, action) => {},
@@ -20,6 +30,7 @@ export const journalSlice = createSlice({
 
 // Action creators are generated for each case reducer function
 export const {
+  savingNewNote,
   addNewEmptyNote,
   setActiveNote,
   setNotes,
--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,9 +1,18 @@
 import { collection, doc, setDoc } from 'firebase/firestore/lite';
 import { FirebaseDB } from '../../firebase/config';
+import {
+  addNewEmptyNote,
+  savingNewNote,
+  setActiveNote,
+  setNotes,
+} from './journalSlice';
+import { loadNotes } from '../../helpers';
 
 export const startNewNote = () => {
   return async (dispatch, getState) => {
     const { uid } = getState().auth;
+
+    dispatch(savingNewNote());
 
     const newNote = {
       title: '',
@@ -12,6 +21,22 @@ export const startNewNote = () => {
     };
 
     const newDoc = doc(collection(FirebaseDB, `${uid}/journal/notes`));
-    const resp = await setDoc(newDoc, newNote);
+    await setDoc(newDoc, newNote);
+
+    newNote.id = newDoc.id;
+
+    dispatch(addNewEmptyNote(newNote));
+    dispatch(setActiveNote(newNote));
+  };
+};
+
+export const startLoadingNotes = () => {
+  return async (dispatch, getState) => {
+    const { uid } = getState().auth;
+    if (!uid) throw new Error('El UID del usuario no existe');
+
+    const notes = await loadNotes(uid);
+
+    dispatch(setNotes(notes));
   };
 };
